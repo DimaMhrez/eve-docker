7b725cc7532765c8df1f1c23bd87724f821601cd
DimaMhrez <38988451+DimaMhrez@users.noreply.github.com>
2019-10-23 16:37:51 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,1 +1,7 @@
+# Let's just use the local mongod instance. Edit as needed.
 
+# Please note that MONGO_HOST and MONGO_PORT could very well be left
+# out as they already default to a bare bones local 'mongod' instance.
+MONGO_HOST = 'localhost'
+MONGO_PORT = 27017
+DOMAIN = {'people': {}}
