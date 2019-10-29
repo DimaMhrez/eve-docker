db88f3465b1e739fbd878319146c6e93ab8d4ee5
DimaMhrez <38988451+DimaMhrez@users.noreply.github.com>
2019-10-29 16:53:24 +0100

    Update evemain.py

--- evemain.py
+++ evemain.py
@@ -2,46 +2,10 @@ from eve import Eve
 
 
 my_settings= {
- 'MONGO_HOST': 'localhost',
+     'MONGO_HOST': 'localhost',
     'MONGO_PORT': 27017,
     'MONGO_DBNAME': 'the_db_name',
-    'DOMAIN': {'people': {}},
-    'RESOURCE_METHODS': ["GET", "POST", "DELETE"],
-    'ITEM_METHODS' : ["GET", "PATCH", "DELETE", "PUT"],
-    'schema' : {
-    # Schema definition, based on Cerberus grammar. Check the Cerberus project
-    # (https://github.com/pyeve/cerberus) for details.
-    'firstname': {
-        'type': 'string',
-        'minlength': 1,
-        'maxlength': 10,
-    },
-    'lastname': {
-        'type': 'string',
-        'minlength': 1,
-        'maxlength': 15,
-        'required': True,
-        # talk about hard constraints! For the purpose of the demo
-        # 'lastname' is an API entry-point, so we need it to be unique.
-        'unique': True,
-    },
-    # 'role' is a list, and can only contain values from 'allowed'.
-    'role': {
-        'type': 'list',
-        'allowed': ["author", "contributor", "copy"],
-    },
-    # An embedded 'strongly-typed' dictionary.
-    'location': {
-        'type': 'dict',
-        'schema': {
-            'address': {'type': 'string'},
-            'city': {'type': 'string'}
-        },
-    },
-    'born': {
-        'type': 'datetime',
-    },
-}
+    'DOMAIN': {'contacts': {}}
 
 }
 app = Eve(settings=my_settings)
