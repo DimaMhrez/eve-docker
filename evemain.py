193ffe511903640b1235d38c9b869968690507e5
DimaMhrez <38988451+DimaMhrez@users.noreply.github.com>
2019-10-29 15:46:26 +0100

    Update evemain.py

--- evemain.py
+++ evemain.py
@@ -1,11 +1,6 @@
 from eve import Eve
 
-my_settings = {
-    'MONGO_HOST': 'localhost',
-    'MONGO_PORT': 27017,
-    'MONGO_DBNAME': 'the_db_name',
-    'DOMAIN': {'contacts': {}}
-}
 
-app = Eve(settings=my_settings)
+
+app = Eve(settings='my_settings')
 app.run(debug=True)
