acae327d025c55d1e4ac87b9feaebed7727bb307
DimaMhrez <38988451+DimaMhrez@users.noreply.github.com>
2019-11-13 12:00:53 +0100

    Update evemain.py

--- evemain.py
+++ evemain.py
@@ -1,13 +1,6 @@
 from eve import Eve
 
 
-my_settings = {
-    'MONGO_HOST': 'mongodb',
-    'MONGO_PORT': 27017,
-    'MONGO_DBNAME': 'the_db_name2',
-    'DOMAIN': {'people': {}},
-    'RESOURCE_METHODS' :['GET', 'POST', 'DELETE']
-}
 
-app = Eve(settings=my_settings)
+app = Eve(settings='my_settings.py')
 app.run(debug=True)
