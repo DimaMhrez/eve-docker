d3b386410d5996feb5fc9aba596063e9900ae933
DimaMhrez <38988451+DimaMhrez@users.noreply.github.com>
2019-11-13 11:34:56 +0100

    Update my_settings.py

--- my_settings.py
+++ my_settings.py
@@ -2,44 +2,35 @@
 
 # Please note that MONGO_HOST and MONGO_PORT could very well be left
 # out as they already default to a bare bones local 'mongod' instance.
-MONGO_HOST = 'localhost'
+MONGO_HOST= 'localhost'
 MONGO_PORT = 27017
-MONGO_DBNAME: 'the_db_name',
-   
-DOMAIN = {'people': people,}
-RESOURCE_METHODS = ["GET", "POST", "DELETE"]
-ITEM_METHODS = ["GET", "PATCH", "DELETE", "PUT"]
-schema = {
-    # Schema definition, based on Cerberus grammar. Check the Cerberus project
-    # (https://github.com/pyeve/cerberus) for details.
-    'firstname': {
-        'type': 'string',
-        'minlength': 1,
-        'maxlength': 10,
-    },
-    'lastname': {
-        'type': 'string',
-        'minlength': 1,
-        'maxlength': 15,
-        'required': True,
-        # talk about hard constraints! For the purpose of the demo
-        # 'lastname' is an API entry-point, so we need it to be unique.
-        'unique': True,
-    },
-    # 'role' is a list, and can only contain values from 'allowed'.
-    'role': {
-        'type': 'list',
-        'allowed': ["author", "contributor", "copy"],
-    },
-    # An embedded 'strongly-typed' dictionary.
-    'location': {
-        'type': 'dict',
-        'schema': {
-            'address': {'type': 'string'},
-            'city': {'type': 'string'}
-        },
-    },
-    'born': {
-        'type': 'datetime',
-    },
+MONGO_DBNAME='dbgettest'
+
+# Enable reads (GET), inserts (POST) and DELETE for resources/collections
+# (if you omit this line, the API will default to ['GET'] and provide
+# read-only access to the endpoint).
+RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
+
+# Enable reads (GET), edits (PATCH) and deletes of individual items
+# (defaults to read-only item access).
+ITEM_METHODS = ['GET', 'PATCH', 'DELETE']
+
+# We enable standard client cache directives for all resources exposed by the
+# API. We can always override these global settings later.
+CACHE_CONTROL = 'max-age=200'
+CACHE_EXPIRES = 200
+
+# Our API will expose two resources (MongoDB collections): 'people' and works
+
+import json
+
+jsonstring=open("my_schema.json").read()
+
+
+response =json.loads(jsonstring)
+
+
+DOMAIN = {
+    'response': response,
+   # 'works': works,
 }
