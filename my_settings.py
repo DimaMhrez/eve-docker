b47a5731fccbad899b5d08e29b49cf60ec2ad5f2
DimaMhrez <38988451+DimaMhrez@users.noreply.github.com>
2019-11-14 10:27:53 +0100

    Update my_settings.py

--- my_settings.py
+++ my_settings.py
@@ -2,7 +2,7 @@
 
 # Please note that MONGO_HOST and MONGO_PORT could very well be left
 # out as they already default to a bare bones local 'mongod' instance.
-MONGO_HOST= 'localhost'
+MONGO_HOST= 'mongodb'
 MONGO_PORT = 27017
 MONGO_DBNAME='dbgettest'
 
